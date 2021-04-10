194b8983314feb4bb225dfd75ce47d862fdefac2
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-04-09 22:41:27 -0400

    first test

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,9 +3,9 @@ const {
 } = require('../models');
 
 const UserController = {
-  // get all pizzas
+  // get all users
   getAllUser(req, res) {
-    Pizza.find({})
+    User.find({})
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
@@ -13,15 +13,15 @@ const UserController = {
       });
   },
 
-  // get one pizza by id
-  getPizzaById({
+  // get one user by id 18.1.6
+  getUserById({
     params
   }, res) {
-    Pizza.findOne({
+    User.findOne({
         _id: params.id
       })
       .then(dbUserData => {
-        // If no pizza is found, send 404
+        // If no User is found, send 404
         if (!dbUserData) {
           res.status(404).json({
             message: 'No users found with this id!'
@@ -35,7 +35,41 @@ const UserController = {
         res.status(400).json(err);
       });
   },
+
+// createUser
+createUser({ body }, res) {
+User.create(body)
+    .then(deUserData => res.json(deUserData))
+    .catch(err => res.status(400).json(err));
+},
+
+  // update User by id 18.5.3 runvalidators
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runvalidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No User found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // delete User
+deleteUser({ params }, res) {
+  User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No User found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
 }
+
+};
 
 
 module.exports = UserController;
